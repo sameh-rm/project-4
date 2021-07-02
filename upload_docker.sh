782fd904a1ca04e3bc7dd7bae6a9e4d2dfb2fb23
Sameh Ramadan <sameh.r.sayed@gmail.com>
2021-07-02 19:00:43 +0200

    updated config

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=urokai/udacity-cloud-devops-project-4
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login -u urokai
+docker tag api $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
