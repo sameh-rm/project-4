6e47c650b8d232ae61e1a75fb4ed009480b419ef
Sameh Ramadan <sameh.r.sayed@gmail.com>
2021-07-02 21:54:46 +0200

    updated config

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True)
